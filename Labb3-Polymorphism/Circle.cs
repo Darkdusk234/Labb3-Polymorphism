86ea3dd8e13ea869747d588ece014930da6b297a
darkdusk234 <johan.svensson.01@hotmail.com>
2024-10-30 10:02:24 +0100

    Created override of Area method in Circle class

--- Labb3-Polymorphism/Circle.cs
+++ Labb3-Polymorphism/Circle.cs
@@ -1,6 +1,13 @@
 ﻿namespace Labb3_Polymorphism
 {
-    internal class Circle
+    internal class Circle : Geometry
     {
+        public double Radius { get; set; }
+        private double _pi = 3.14;
+
+        public override double Area()
+        {
+            return Radius * Radius * _pi;
+        }
     }
 }
