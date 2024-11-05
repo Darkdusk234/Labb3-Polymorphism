460ff1abdf5959599f3337b6becfca7c4dc2f6eb
darkdusk234 <johan.svensson.01@hotmail.com>
2024-11-05 09:53:35 +0100

    Created loop that goes through a list of forms to print out area and circumference of the forms.

--- Labb3-Polymorphism/Program.cs
+++ Labb3-Polymorphism/Program.cs
@@ -4,7 +4,22 @@
     {
         static void Main(string[] args)
         {
+            List<Geometry> forms = new List<Geometry>();
+            Geometry circle = new Circle(7.3);
+            forms.Add(circle);
+            Geometry rectangle = new Rectangle(8.5, 5.7);
+            forms.Add(rectangle);
+            Geometry square = new Square(9.2);
+            forms.Add(square);
+            Geometry triangle = new Triangel(9.4, 7.1);
+            forms.Add(triangle);
 
+            foreach (Geometry form in forms)
+            {
+                Console.WriteLine($"Area of {form.FormType()}: {Math.Round(form.Area(), 2)}");
+                Console.WriteLine($"Circumference of {form.FormType()}: {Math.Round(form.Circumference(), 2)}");
+                Console.WriteLine();
+            }
         }
     }
 }
