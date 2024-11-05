5799f9e412014f0fb2e6c798931a7c50594df946
darkdusk234 <johan.svensson.01@hotmail.com>
2024-11-05 09:06:41 +0100

    Created override of Area() method in Triangel class

--- Labb3-Polymorphism/Triangel.cs
+++ Labb3-Polymorphism/Triangel.cs
@@ -1,6 +1,13 @@
 ﻿namespace Labb3_Polymorphism
 {
-    internal class Triangel
+    internal class Triangel : Geometry
     {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public override double Area()
+        {
+            return (Width * Height) / 2;
+        }
     }
 }
